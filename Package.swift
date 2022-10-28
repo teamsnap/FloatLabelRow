583c1202aa31a40df1a27ada7b0a880fde726aed
Alejandro Moya <ale.moya@teamsnap.com>
2022-10-28 12:16:41 -0300

    fixed path issue

--- Package.swift
+++ Package.swift
@@ -19,9 +19,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FloatLabelRow",
+            path: "Sources",
             dependencies: ["Eureka"]),
         .testTarget(
             name: "FloatLabelRowTests",
+            path: "Tests",
             dependencies: ["FloatLabelRow"]),
     ]
 )
